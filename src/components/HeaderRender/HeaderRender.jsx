022917a323cdbff2b645c77a30aba92869112760
Maxim <max.spivakov@gmail.com>
2018-12-04 01:31:19 +0300

    Header refactoring

--- src/components/HeaderRender/HeaderRender.jsx
+++ src/components/HeaderRender/HeaderRender.jsx
@@ -10,11 +10,11 @@ export default class Header extends Component {
                     Browse Available Books
                 </h1>
                 <div className={ "header__book-filter book-filter" }>
-                    <div className={ "book-filter__link-box radio_js" }>
-                        <button className={ "book-filter__link book-filter__link_focus" } disabled>All Books</button>
-                        <button className={ "book-filter__link" }>Most Recent</button>
-                        <button className={ "book-filter__link" }>Most Popular</button>
-                        <button className={ "book-filter__link" }>Free Books</button>
+                    <div className={ "book-filter__button-box filter-panel_js" }>
+                        <button className={ "book-filter__button" }>All Books</button>
+                        <button className={ "book-filter__button" }>Most Recent</button>
+                        <button className={ "book-filter__button" }>Most Popular</button>
+                        <button className={ "book-filter__button" }>Free Books</button>
                     </div>
                     <div className={ "book-filter__search-field search-field" }>
                         <input className={ "search-field__input" } placeholder={ "Enter Keywords" }/>
